09bb199fcf36c95eb33eeadc9b85ac2c4ddc51ab
nagaraj-nex <82175614+nagaraj-nex@users.noreply.github.com>
2021-04-17 00:36:31 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,10 +12,10 @@ echo $INPUT_SERVER_IP $INPUT_SERVER_NAME |  tee -a /etc/hosts
 # Run `base64 -i yourCA.pem`. Copy the output of into the Github secret `BFE_SSL_CERTIFICATE` of your repository and pass that as input
 # to the Github action
 
-echo $INPUT_SSL_CERTIFICATE > /scripts/serverCA_base64
-base64 --decode /scripts/serverCA_base64 > /scripts/serverCA.pem
-echo "Copied user SSL cert into file, installing it"
-python /scripts/install_cert_python.py --cert serverCA.pem
+#echo $INPUT_SSL_CERTIFICATE > /scripts/serverCA_base64
+#base64 --decode /scripts/serverCA_base64 > /scripts/serverCA.pem
+#echo "Copied user SSL cert into file, installing it"
+#python /scripts/install_cert_python.py --cert serverCA.pem
 
 # Upload snapshot to Batfish Enterprise server
 python /scripts/upload_snapshot.py
